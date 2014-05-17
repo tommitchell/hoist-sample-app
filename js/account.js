d8b8fbe433844a7106532a0e65e37eb7d556bbcb
Jamie Wilson <jamie.wilsons@gmail.com>
2014-05-16 22:13:26 -0700

    uploads first version of the sample app

---
+++ js/account.js
@@ -0,0 +1,105 @@
+var AccountManager = function() {
+
+
+};
+
+AccountManager.prototype = {
+
+  drawLogin: function() {
+
+    //Load the login template
+    $("#content").html(_.template($("#login").html())());
+
+    $("form[name='login']").on("submit", function(evt) {
+
+      //Kill the login button
+      $("#login-button").attr("disabled", "disabled");
+
+      evt.preventDefault();
+
+      account.login(
+        $("input[name='username']").val(),
+        $("input[name='password']").val(),
+        function(member) {
+          app = new App(member);
+        },
+        function() {
+          alert("Sorry, you can't come in here.");
+          $("#login-button").removeAttr("disabled");
+        }
+      );
+
+      return false;
+    });
+
+    $("form[name='signup']").on("submit", function(evt) {
+
+      evt.preventDefault();
+
+      //Kill the login button
+      $("#signup-button").attr("disabled", "disabled");
+
+      account.new(
+        $("input[name='name']").val(),
+        $("input[name='emailAddress']").val(),
+        $("input[name='signupPassword']").val(),
+        $("input[name='profilePhoto']"),
+        function(member) {
+
+          app = new App(member);
+
+        }, function(message) {
+
+          alert(message || "Signup failed, sorry.");
+          $("#signup-button").removeAttr("disabled");
+
+        }
+      )
+
+      return false;
+
+    });
+
+
+  },
+
+
+  login: function(email, password, success, failure) {
+
+    Hoist.login({
+      email: email,
+      password: password
+    }, success, failure);
+
+  },
+
+  new: function(name, email, password, $profilePhoto, success, failure) {
+
+    var user = {
+      name: name,
+      email: email,
+      password: password
+    };
+    if($profilePhoto.val()) {
+      //Upload their profile pic
+      Hoist.file(user.email, $profilePhoto, function(success) {
+        //sign them up
+        Hoist.signup(user, function(member) {
+
+          Hoist("members").post({
+            name: user.name,
+            emailAddress: user.email,
+            id: member.id
+          }, function() {
+            app = new App(member);
+          });
+
+        });
+      });
+
+    } else {
+      failure("Sorry, you need a profile photo!");
+    }
+
+  }
+};
---
+++ js/app.js
@@ -0,0 +1,195 @@
+var app, key;
+$(function() {
+
+	var settingsLocation = window.location.href.indexOf("localhost") !== -1 ? "./hoist.json" : "/settings";
+
+	$.getJSON(settingsLocation, function(settings) {
+
+		Hoist.apiKey(settings.apiKey);
+		key = settings.apiKey;
+
+		//Account Controller
+		window.account = new AccountManager();
+
+		//If logged in, start the app, or show the login screen
+		Hoist.status(function(member) {
+			app = new App(member);
+		}, function() {
+			account.drawLogin();
+		});
+
+	});
+
+});
+
+var App = function(member) {
+
+	var self = this,
+			structure = _.template($("#page_structure").html()),
+			body = _.template($("#page_body").html()),
+			navigation = _.template($("#page_navigation").html());
+
+	this.member = member;
+
+	//Draw the page content
+	$("#content").html(structure()).append(body());
+
+	//The Hoist "Data Manager" doesn't store the result of a collection
+	//on get, so we have two separate objects to update
+	this._posts = Hoist("posts");
+	this.posts;
+
+	this._members = Hoist("members");
+	this.members;
+
+	//Get the members
+	this._members.get(function(res) {
+
+		$("nav .content").append(navigation(self));
+
+		//Store the members in the app
+		self.members = res;
+
+		//get the member who is just logged in
+		self.member = _.find(self.members, function(m) {
+			return m.id === self.member.id;
+		});
+
+		//Attach event handlers
+		self.attachEvents();
+
+		self.loadPosts();
+
+
+	});
+
+};
+
+_.extend(App.prototype, {
+
+	attachEvents: function() {
+		//attach event handlers
+		$("textarea")
+			.on("focus", this.focusTextarea)
+			.on("blur", this.blurTextarea);
+
+		$("#post-status")
+			.on("click", this.postStatus);
+
+		$("body")
+			.on("click", ".js-delete-post", this.deleteStatus);
+
+		$("body")
+			.on("click", ".js-logout", this.logout);
+	},
+
+	focusTextarea: function() {
+
+		$("#comments-actions").show();
+		$("#preview").show();
+		if($("textarea").val() === "Write something here") {
+			$("textarea").val('');
+		}
+
+	},
+	blurTextarea: function(e) {
+
+		if($("textarea").val() == '') { $("textarea").val('Write something here'); }
+		if($(e.relatedTarget).attr("id") === "post-status") {
+
+		} else {
+			$("#comments-actions").hide();
+			$("#preview").hide();
+		}
+
+	},
+	postStatus: function() {
+
+		var content = $("textarea").val();
+		if(content == "") return;
+		$("textarea").val("Write something here");
+
+		var post = {
+			text: strip(content),
+			media: "",
+			ownerId: app.member.id
+		};
+
+		app._posts.post(post);
+		app.drawPost(post);
+
+
+		$("#comments-actions").hide();
+		$("#preview").hide();
+
+	},
+
+	logout: function() {
+		Hoist.logout(function() {
+			account.drawLogin();
+		});
+	},
+
+	getUser: function(id) {
+
+		var user = _.find(this.members, function(m) {
+			return m.id === id;
+		});
+		return user;
+
+	},
+
+	loadPosts: function(success) {
+
+		var self = this;
+		this._posts.get(function(res) {
+			//Store the posts in the app
+			self.posts = res;
+			$(".loading").remove();
+			//Loop through the posts to draw them
+			_.each(res, function(r) {
+				self.drawPost(r);
+			}, this);
+		});
+
+	},
+
+	drawPost: function(post) {
+
+		//making it one level deep to make underscore templates easier
+		//to work with
+		post.post = post;
+		post.post.markedDownText = marked(post.post.text);
+		//Get the user from the members array
+		post.user = this.getUser(post.ownerId) || {};
+		post.isEditable = post.ownerId === this.member.id;
+
+		var template = _.template($("#post").html());
+		var post_html = template(post);
+		$("#content-placeholder").prepend(post_html);
+
+	},
+
+	deleteStatus: function() {
+
+		var c = confirm("Are you sure you want to delete this post?");
+		if(c) {
+			//get the id
+			var id = $(this).data("post");
+			//delete the post
+			$(".media[data-post='"+ id +"']").remove();
+			//delete from the app
+			app._posts.remove(id);
+		}
+
+	}
+});
+
+
+//Helpers
+function strip(html)
+{
+	var tmp = document.createElement("DIV");
+	tmp.innerHTML = html;
+	return tmp.textContent || tmp.innerText || "";
+}
